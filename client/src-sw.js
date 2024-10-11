07aad4cfab8329eedf8a8f2782596b824889e20d
shanwc1972 <166612646+shanwc1972@users.noreply.github.com>
2024-10-11 22:32:30 +1100

    Custom service worker config and webpack config

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+//Setup asset caching
+registerRoute(
+  // Callback function to cache JS, CSS and WORKER files
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
